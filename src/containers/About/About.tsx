7d7d0d56c21a82c0df5749674d75d53142985ce6
Aibek Saaliev <dokaidea@gmail.com>
2022-12-03 09:06:19 +0600

    added css styles for About and Contacts components

--- src/containers/About/About.tsx
+++ src/containers/About/About.tsx
@@ -2,6 +2,7 @@ import React, {FormEvent, useCallback, useEffect, useState} from 'react';
 import Container from "react-bootstrap/Container";
 import axiosApi from "../../axiosApi";
 import {AboutInfoType} from "../../types";
+import ReactTooltip from "react-tooltip";
 
 const About = () => {
   const [aboutInfo, setAboutInfo] = useState<AboutInfoType>({
@@ -44,19 +45,33 @@ const About = () => {
 
 
   return (
-    <Container>
+    <Container className="text-center pt-4 pb-4 position-relative">
       <h4>About</h4>
       <h5>History</h5>
       <p>{aboutInfo?.history}</p>
       <h5>Mission</h5>
       <p>{aboutInfo?.mission}</p>
-      <span>Established:{aboutInfo?.established}</span>
-      <span>How many posts i wrote:{aboutInfo?.posts}</span>
-      <button className="btn btn-dark" onClick={() => setEditShow(true)}>Edit</button>
+      <span className="d-block">Established: {aboutInfo?.established}.</span>
+      <span className="d-block">How many posts i wrote: {aboutInfo?.posts}.</span>
+      <button
+        className="btn btn-dark d-block position-absolute top-0 end-0 mt-4 rounded-circle"
+        onClick={() => setEditShow(true)}
+        style={{padding: "6px 10px"}}
+        data-tip="" data-for="edit_about"
+      >
+        <i className="bi bi-three-dots-vertical"></i>
+      </button>
+      <ReactTooltip
+        id="edit_about"
+        place="left"
+        effect="solid"
+      >
+        Edit page
+      </ReactTooltip>
       {editShow ? (
-        <form onSubmit={onFormSubmit}>
+        <form onSubmit={onFormSubmit} className="mt-3">
           <div className="form-group">
-            <label htmlFor="exampleFormControlInput1">History</label>
+            <label htmlFor="exampleFormControlInput1">Edit history info</label>
             <textarea
               className="form-control bg-transparent text-dark"
               required
@@ -66,7 +81,7 @@ const About = () => {
             />
           </div>
           <div className="form-group">
-            <label htmlFor="exampleFormControlInput1">Mission</label>
+            <label htmlFor="exampleFormControlInput1">Edit mission info</label>
             <textarea
               className="form-control bg-transparent text-dark"
               required
@@ -76,7 +91,7 @@ const About = () => {
             />
           </div>
           <div className="form-group">
-            <label htmlFor="exampleFormControlInput1">History</label>
+            <label htmlFor="exampleFormControlInput1">Edit established info</label>
             <input
               type="text"
               className="form-control bg-transparent text-dark"
@@ -87,7 +102,7 @@ const About = () => {
             />
           </div>
           <div className="form-group">
-            <label htmlFor="exampleFormControlInput1">History</label>
+            <label htmlFor="exampleFormControlInput1">Edit posts info</label>
             <input
               type="text"
               className="form-control bg-transparent text-dark"
@@ -98,7 +113,7 @@ const About = () => {
             />
           </div>
           <div className="form-group">
-            <button type="submit" className="btn btn-dark">Save</button>
+            <button type="submit" className="btn btn-dark mt-2">Save</button>
           </div>
         </form>
       ) : null}
--- src/containers/Contacts/Contacts.tsx
+++ src/containers/Contacts/Contacts.tsx
@@ -2,6 +2,7 @@ import React, {FormEvent, useCallback, useEffect, useState} from 'react';
 import Container from "react-bootstrap/Container";
 import {AboutInfoType, ContactsInfoType} from "../../types";
 import axiosApi from "../../axiosApi";
+import ReactTooltip from "react-tooltip";
 
 const Contacts = () => {
   const [contactsInfo, setContactsInfo] = useState<ContactsInfoType>({
@@ -41,15 +42,29 @@ const Contacts = () => {
   };
 
   return (
-    <Container>
-      <h5>Contacts</h5>
-      <span>Phone: {contactsInfo.phone}</span>
-      <span>E-mail: {contactsInfo.email}</span>
-      <button className="btn btn-dark" onClick={() => setEditShow(true)}>Edit</button>
+    <Container className="text-center pt-4 pb-4 position-relative">
+      <h5 className="mb-3">Contacts</h5>
+      <span className="d-block">Phone: <b>{contactsInfo.phone}</b></span>
+      <span className="d-block">E-mail: <b>{contactsInfo.email}</b></span>
+      <button
+        className="btn btn-dark d-block position-absolute top-0 end-0 mt-4 rounded-circle"
+        onClick={() => setEditShow(true)}
+        style={{padding: "6px 10px"}}
+        data-tip="" data-for="edit_contacts"
+      >
+        <i className="bi bi-three-dots-vertical"></i>
+      </button>
+      <ReactTooltip
+        id="edit_contacts"
+        place="left"
+        effect="solid"
+      >
+        Edit page
+      </ReactTooltip>
       {editShow ? (
-        <form onSubmit={onFormSubmit}>
+        <form onSubmit={onFormSubmit} className="mt-3">
           <div className="form-group">
-            <label htmlFor="exampleFormControlInput1">History</label>
+            <label htmlFor="exampleFormControlInput1">Edit phone info</label>
             <input
               type="text"
               className="form-control bg-transparent text-dark"
@@ -60,7 +75,7 @@ const Contacts = () => {
             />
           </div>
           <div className="form-group">
-            <label htmlFor="exampleFormControlInput1">History</label>
+            <label htmlFor="exampleFormControlInput1">Edit email info</label>
             <input
               type="text"
               className="form-control bg-transparent text-dark"
@@ -70,7 +85,7 @@ const Contacts = () => {
               onChange={infoChanged}
             />
           </div>
-          <button type="submit" className="btn btn-dark">Save</button>
+          <button type="submit" className="btn btn-dark mt-2">Save</button>
         </form>
       ) : null}
     </Container>
